67df09008daf1c750ff0b81297533d82b06f4ade
gajewsk2 <gajewsk2@illinois.edu>
2015-08-16 13:49:15 -0500

    Successfully parsing all xml files to csv

--- parse_language.py
+++ parse_language.py
@@ -1,6 +1,13 @@
+import os
+from os import listdir
+from os.path import isfile, join
 from lxml import etree
-import csv
+
 from unicode_writer import UnicodeWriter
+
+def get_files(folder):
+    files = [f for f in listdir(folder) if isfile(join(folder, f))]
+    return files
 
 def get_tags(input):
     doc = etree.parse(input)
@@ -8,28 +15,44 @@ def get_tags(input):
     return tags
 
 
-def parse_xml(primary, source, output):
-    primary_tags = get_tags(primary)
+def sanitize(text):
+    if text is not None:
+        return text.strip(' \t\n\r')
+    return ''
+def parse_xml(target, source, output):
+    target_tags = get_tags(target)
     source_tags = get_tags(source)
 
     # with open(output, 'a+') as csv_file:
-    with open(output, 'wb') as fout:
-        for p, s in zip(primary_tags, source_tags):
-            if p.get('name') == s.get('name'):
+    with open(output, 'a+') as fout:
+        for t, s in zip(target_tags, source_tags):
+            if t.get('name') == s.get('name'):
+                    t_text = sanitize(t.text)
+                    s_text = sanitize(s.text)
+                    if t_text != '' and s_text != ''\
+                            and t_text != s_text:
+                        writer = UnicodeWriter(fout)
+                        writer.writerow([t_text, s_text])
 
-                # reader = UnicodeReader(fin)
-                writer = UnicodeWriter(fout)
-                writer.writerow([p.text, s.text])
-                # w = csv.writer(utf_8_encoder(csv_file))
-                # w.writerow([p.text, s.text])
+
+def parse_folder(target, source, output):
+    target_files = get_files(target)
+    for t in target_files:
+        parse_xml(target + t, source + t, output)
 
 
-def utf_8_encoder(unicode_csv_data):
-    for line in unicode_csv_data:
-        yield line.encode('utf-8')
+def remove_file(filename):
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
 
 def main():
-    parse_xml("ja/01-core.xml", "eu/01-core.xml", "output/1.csv")
+    output = 'output/2.csv'
+    remove_file(output)
+    parse_folder('ja/', 'eu/', output)
+    
 
 if __name__ == '__main__':
     main()
